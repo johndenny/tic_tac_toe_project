7d4f6e0daa8adf673c0fe7b43341041c13c10f24
John Denny <johnnydenny@gmail.com>
2021-12-07 00:37:31 -0500

    added pop up message with winner, click on pop up container to clear board and start new game.

--- script.js
+++ script.js
@@ -17,24 +17,41 @@ const gameBoard = {
     },
     winnerCheck: () => {
         let arry = gameBoard.gamePiecesArry;
-        arry[0] === 'x' && arry[1] === 'x' && arry[2] === 'x' ? console.log('Winner'): false;
-        arry[3] === 'x' && arry[4] === 'x' && arry[5] === 'x' ? console.log('Winner'): false;
-        arry[6] === 'x' && arry[7] === 'x' && arry[8] === 'x' ? console.log('Winner'): false;
-        arry[0] === 'x' && arry[3] === 'x' && arry[6] === 'x' ? console.log('Winner'): false;
-        arry[1] === 'x' && arry[4] === 'x' && arry[7] === 'x' ? console.log('Winner'): false;
-        arry[2] === 'x' && arry[5] === 'x' && arry[8] === 'x' ? console.log('Winner'): false;
-        arry[0] === 'x' && arry[4] === 'x' && arry[8] === 'x' ? console.log('Winner'): false;
-        arry[2] === 'x' && arry[4] === 'x' && arry[6] === 'x' ? console.log('Winner'): false;
-        arry[0] === 'o' && arry[1] === 'o' && arry[2] === 'o' ? console.log('Winner'): false;
-        arry[3] === 'o' && arry[4] === 'o' && arry[5] === 'o' ? console.log('Winner'): false;
-        arry[6] === 'o' && arry[7] === 'o' && arry[8] === 'o' ? console.log('Winner'): false;
-        arry[0] === 'o' && arry[3] === 'o' && arry[6] === 'o' ? console.log('Winner'): false;
-        arry[1] === 'o' && arry[4] === 'o' && arry[7] === 'o' ? console.log('Winner'): false;
-        arry[2] === 'o' && arry[5] === 'o' && arry[8] === 'o' ? console.log('Winner'): false;
-        arry[0] === 'o' && arry[4] === 'o' && arry[8] === 'o' ? console.log('Winner'): false;
-        arry[2] === 'o' && arry[4] === 'o' && arry[6] === 'o' ? console.log('Winner'): false;  
+        arry[0] === 'x' && arry[1] === 'x' && arry[2] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[3] === 'x' && arry[4] === 'x' && arry[5] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[6] === 'x' && arry[7] === 'x' && arry[8] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[0] === 'x' && arry[3] === 'x' && arry[6] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[1] === 'x' && arry[4] === 'x' && arry[7] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[2] === 'x' && arry[5] === 'x' && arry[8] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[0] === 'x' && arry[4] === 'x' && arry[8] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[2] === 'x' && arry[4] === 'x' && arry[6] === 'x' ? gameBoard.winnerMessage(): false;
+        arry[0] === 'o' && arry[1] === 'o' && arry[2] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[3] === 'o' && arry[4] === 'o' && arry[5] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[6] === 'o' && arry[7] === 'o' && arry[8] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[0] === 'o' && arry[3] === 'o' && arry[6] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[1] === 'o' && arry[4] === 'o' && arry[7] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[2] === 'o' && arry[5] === 'o' && arry[8] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[0] === 'o' && arry[4] === 'o' && arry[8] === 'o' ? gameBoard.winnerMessage(): false;
+        arry[2] === 'o' && arry[4] === 'o' && arry[6] === 'o' ? gameBoard.winnerMessage(): false;  
+    },
+    winnerMessage: () => {
+        let msg = document.querySelector('#msg');
+        gameBoard.playerTurn === 'playerOne' ? 
+            msg.innerHTML = 'O is the Winner' :
+            msg.innerHTML = 'X is the Winner' ;
+        let popup = document.querySelector('#msgCntr');
+        popup.style.display = 'flex';
+    },
+    newGame: () => {
+        gameBoard.gamePiecesArry = ['','','','','','','','',''];
+        gameBoard.gamePiecePrint();
+        let popup = document.querySelector('#msgCntr');
+        popup.style.display = 'none';
     }
 }
+
+const popUpBckgrd = document.querySelector('#msgCntr');
+popUpBckgrd.addEventListener('click', gameBoard.newGame);
 
 const gamePeice = document.querySelectorAll('#boardSection');
 
